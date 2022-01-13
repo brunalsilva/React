2ad4c03f90b25bfedb12e236d0fb60417171d565
Bruna Silva <beyss21@gmail.com>
2022-01-13 14:07:56 -0300

    Atividade react - blog pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="Foto do universo" className='img'/>
+        </>
+    );
+}
+
+export default Home;
