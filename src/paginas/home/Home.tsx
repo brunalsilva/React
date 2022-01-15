81df60bce74315ae723bbbad2bea2971d8643113
Bruna Silva <beyss21@gmail.com>
2022-01-14 21:33:40 -0300

    Add material UI

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Home.css';
+import Button from '@mui/material/Button';
 
 function Home() {
     return (
         <>
             <h1 className='titulo'>Home</h1>
             <img src="https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="Foto do universo" className='img'/>
+
+            <Button variant="contained" disableElevation>
+                Disable elevation
+            </Button>
         </>
     );
 }
